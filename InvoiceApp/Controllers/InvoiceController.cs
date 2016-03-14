6038bb6b5e79a3f2983f96711797d1df2a088805
Malthe Friisberg <spamthisnoob@hotmail.com>
2016-03-14 21:25:19 +0100

    1st commit

---
+++ InvoiceApp/Controllers/InvoiceController.cs
@@ -0,0 +1,33 @@
+﻿using InvoiceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace InvoiceApp.Controllers
+{
+    public class InvoiceController : ApiController
+    {
+        Invoice[] invoices = new Invoice[]
+        {
+            new Invoice {id = 0, name = "hans" },
+            new Invoice {id = 1, name = "kurt" }
+        };
+
+        public IEnumerable<Invoice> GetAllInvoices()
+        {
+            return invoices;
+        }
+        public IHttpActionResult GetInvoice(int id)
+        {
+            var invoice = invoices.FirstOrDefault((p) => p.id == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+            return Ok(invoice);
+        }
+    }
+}
---
+++ InvoiceApp/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using InvoiceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvoiceApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Invoices
+        Product[] Products = new Product[]
+        {
+            new Product {id = 0, name = "hans" },
+            new Product {id = 1, name = "kurt" }
+        };
+        //http://localhost:54708/product/GetAllProducts
+        public JsonResult GetAllProducts()
+        {
+            return Json(Products, JsonRequestBehavior.AllowGet);
+        }
+        //http://localhost:54708/product/GetProduct/1
+        public ActionResult GetProduct(int id)
+        {
+            var invoice = Products.FirstOrDefault((p) => p.id == id);
+            if (invoice == null)
+            {
+                return this.Json("error");
+            }
+            return this.Json(invoice, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
